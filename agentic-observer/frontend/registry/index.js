399959e754f04d9a03689bee22835781098f5ffe
Shardul Khandekar <khandekarshardul@gmail.com>
2026-03-05 00:00:00 -0500

    removing everything and starting from scratch

--- agentic-observer/frontend/registry/index.js
+++
@@ -1,9 +0,0 @@
-import { Registry } from 'a2ui';
-import { TaskCard } from '../components/TaskCard.js';
-
-export function initializeRegistry() {
-
-    Registry.add('TaskCardComponent', TaskCard);
-    console.log("A2UI Registry Initialized");
-
-}
